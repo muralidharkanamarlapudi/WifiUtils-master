bcda86f5b7a32fd8a2778bcf293f8574e759fd7c
murali <muralidharan.dharan9@gmail.com>
2018-06-10 13:19:34 +0530

    Changes:
    ===> Simple Dialog Box added on Wifi Item Click

--- sample/src/main/java/com/thanosfisherman/wifiutils/sample/MainActivity.java
+++ sample/src/main/java/com/thanosfisherman/wifiutils/sample/MainActivity.java
@@ -1,11 +1,14 @@
 package com.thanosfisherman.wifiutils.sample;
 
 import android.Manifest;
+import android.content.DialogInterface;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.RequiresApi;
 import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Toast;
@@ -17,10 +20,11 @@ import android.widget.ListView;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
+import android.view.View;
 
 import com.thanosfisherman.wifiutils.WifiUtils;
 public class MainActivity extends AppCompatActivity {
-
+    ArrayList mobileArrayObj = new ArrayList();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -51,7 +55,7 @@ public class MainActivity extends AppCompatActivity {
         }
         Log.i(TAG, "GOT SCAN RESULTS " + results);
 
-        ArrayList mobileArrayObj = new ArrayList();
+
         HashSet hs = new HashSet();
         for (int i=0;i<results.size();i++){
             if(!results.get(i).SSID.isEmpty()){
@@ -68,6 +72,37 @@ public class MainActivity extends AppCompatActivity {
 
         ListView listView = (ListView) findViewById(R.id.mobile_list);
         listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+               /* Toast.makeText(MainActivity.this, mobileArrayObj.get(position), Toast.LENGTH_SHORT).show();*/
+               /*Toast.makeText(MainActivity.this,mobileArrayObj.get(position).toString(),Toast.LENGTH_SHORT);*/
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle("Title os Alert");
+                builder.setMessage("Please Diagnose "+mobileArrayObj.get(position).toString());
+                builder.setCancelable(false);
+
+                builder.setPositiveButton("YES", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(getApplicationContext(),"Yes Button Clicked",Toast.LENGTH_LONG);
+                        dialog.cancel();
+                    }
+                });
+
+                builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(getApplicationContext(),"No Button Clicked",Toast.LENGTH_LONG);
+                        dialog.cancel();
+                    }
+                });
+
+                AlertDialog alert = builder.create();
+                alert.show();
+            }
+        });
 
     }
 
@@ -93,5 +128,12 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(MainActivity.this, "SUCCESS!", Toast.LENGTH_SHORT).show();
         else
             Toast.makeText(MainActivity.this, "EPIC FAIL!", Toast.LENGTH_SHORT).show();
+    }
+
+    public void sendMessage(View view)
+    {
+        // Do something in response to button click
+        Toast.makeText(MainActivity.this, "SUCCESS! xxxxxxxxxxxxxxxxxxxxxxxx", Toast.LENGTH_SHORT).show();
+
     }
 }
